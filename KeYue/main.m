bfd10e2cb7fdb98aae58ae1c7fc7b6a0d8f54581
mingway <ita9178@gmail.com>
2015-11-11 17:21:40 +0800

    Initial Commit

---
+++ KeYue/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KeYue
+//
+//  Created by mingway on 11/11/15.
+//  Copyright © 2015 KeYue Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
