007b48f036d9d7845ad17f5e27a76e0086765e6b
mingway <ita9178@gmail.com>
2015-11-25 21:55:17 +0800

    项目架构调整

--- KeYue/ViewController.m
+++ KeYue/ViewController.m
@@ -17,11 +17,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self setupBackgroundColor];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark setup views
+
+- (void)setupBackgroundColor {
+    self.view.backgroundColor = [UIColor whiteColor];
 }
 
 @end
